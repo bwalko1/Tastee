c02a7f2c05b98ac98907f3463246af80068f2226
bwalko1 <bwalko1@kent.edu>
2019-04-13 16:30:31 -0400

    Setting up the project further, added new content layout

--- app/src/main/java/com/benwalko/tastee/Landing.kt
+++ app/src/main/java/com/benwalko/tastee/Landing.kt
@@ -3,16 +3,13 @@ package com.benwalko.tastee
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log.d
+import kotlinx.android.synthetic.main.content_landing.*
 
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
-
-class MainActivity : AppCompatActivity() {
+class Landing : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_landing)
 
         bigButton.setOnClickListener {
             startActivity(Intent(this, Menu::class.java))
