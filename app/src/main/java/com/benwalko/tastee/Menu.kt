33643db683162ec7a8ceeb79f64e2c6559786df2
bwalko1 <bwalko1@kent.edu>
2019-04-13 11:23:43 -0400

    Initial commit

---
+++ app/src/main/java/com/benwalko/tastee/Menu.kt
@@ -0,0 +1,11 @@
+package com.benwalko.tastee
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
